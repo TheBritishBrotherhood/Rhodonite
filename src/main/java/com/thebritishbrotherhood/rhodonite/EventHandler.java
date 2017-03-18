62e805ec09ca3ab0c28de7c457200c984fd0348d
TheBritishBrotherhood <curtislee1994@live.co.uk>
2017-03-18 14:40:41 +0000

    Oops

--- src/main/java/com/thebritishbrotherhood/rhodonite/EventHandler.java
+++ src/main/java/com/thebritishbrotherhood/rhodonite/EventHandler.java
@@ -1,19 +1,27 @@
  package com.thebritishbrotherhood.rhodonite;
 
+ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
  import net.minecraft.inventory.EntityEquipmentSlot;
+ import net.minecraft.item.Item;
  import net.minecraft.item.ItemStack;
 
+ import net.minecraftforge.client.model.ModelLoader;
+ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
  import net.minecraftforge.fml.common.gameevent.TickEvent;
+ import net.minecraftforge.fml.relauncher.Side;
+ import net.minecraftforge.fml.relauncher.SideOnly;
+
+ import static sun.audio.AudioPlayer.player;
 
 
  public class EventHandler
  {
-   @net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+   @SubscribeEvent
    public void tick(TickEvent.PlayerTickEvent e)
    {
      if (!e.player.getEntityData().hasKey("wearingArmor")) {
-       e.player.getEntityData().setBoolean("wearingArmor", false);
-       e.player.getEntityData().setBoolean("canFly", e.player.capabilities.allowFlying);
+         e.player.getEntityData().setBoolean("wearingArmor", false);
+         e.player.getEntityData().setBoolean("canFly", e.player.capabilities.allowFlying);
      }
 
      ItemStack stack = e.player.inventory.armorItemInSlot(EntityEquipmentSlot.CHEST.getIndex());
